b9a5c07f8331a16345fba25779ca1a13c980a9e6
SethBastianUade <marroyooblitas@uade.edu.ar>
2026-06-16 19:50:33 -0300

    Fix menor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,13 +13,6 @@ public class Main {
 
     public static void main(String[] args) {
         cargarDatosDemo();
-
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception ignored) {
-
-        }
-
         SwingUtilities.invokeLater(() -> new MenuPrincipal().setVisible(true));
     }
 
